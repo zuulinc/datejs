d9b745fad747f2df1f6c6d0bf043f38ab60a9ee1
Keith Foster <keith@zuul.ca>
2015-10-13 14:47:09 -0400

    updating so we're progressively going through the format string to avoid the matches on string segments that are as a result of parsing.

--- datejs.js
+++ datejs.js
@@ -6,50 +6,52 @@
  * @return {string}     the new string
  */
 var pad = function(num) {
-	num = num.toString().split('');
-	var defaults = {
-		padTo: 2,
-		position: 0,
-		char: '0'
-	};
+    num = num.toString().split('');
+    var defaults = {
+        padTo: 2,
+        position: 0,
+        char: '0'
+    };
 
-	if (num.length < defaults.padTo) {
-		for (i=num.length; i<defaults.padTo; i++) {
-			num.unshift(defaults.char);
-		}
-	}
+    if (num.length < defaults.padTo) {
+        for (var i=num.length; i<defaults.padTo; i++) {
+            num.unshift(defaults.char);
+        }
+    }
 
-	return num.join('');
+    return num.join('');
 };
 
+/*
+Static Variables
+ */
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-	mths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
-	dys = ['Sun', 'Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat'],
-	days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    mths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+    dys = ['Sun', 'Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat'],
+    days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-// datejs.format(date, 'MM')
 var __ = {
-	F: function(d) {
-		return months[d.getMonth()];
-	},
-	m: function(d) {
-		return pad(d.getMonth()+1);
-	},
-	M: function(d) {
-		return mths[d.getMonth()];
-	},
-	Y: function(d) {
-		return d.getFullYear();
-	},
-	d: function(d) {
-		return pad(d.getDate());
-	},
-	D: function(d) {
-		return dys[d.getDay()];
-	},
-	l: function(d) {
-		return days[d.getDay()];
-	}
+    F: function(d) {
+        return months[d.getMonth()];
+    },
+    m: function(d) {
+        return pad(d.getMonth()+1);
+    },
+    M: function(d) {
+        return mths[d.getMonth()];
+    },
+    Y: function(d) {
+        return d.getFullYear();
+    },
+    d: function(d) {
+        return pad(d.getDate());
+    },
+    D: function(d) {
+        return dys[d.getDay()];
+    },
+    l: function(d) {
+        return days[d.getDay()];
+    }
 };
 
 /**
@@ -58,117 +60,131 @@ var __ = {
  */
 module.exports = {
 
-	/*
-	Default of course is 0, but if you want to change it to something different you can do so here.
-	 */
-	weekStartsOn: 1, // monday
+    /*
+    Default of course is 0, but if you want to change it to something different you can do so here.
+     */
+    weekStartsOn: 1, // monday
 
-	/**
-	 * Checks if a date is in this week
-	 * @param  {Date}  target date we're looking for
-	 * @param  {Int}  offset day offset, used with the isNextWeek, so that we can use the same logic, but just add 7 to the equations.
-	 * @return {Boolean}
-	 */
-	isThisWeek: function(target, offset) {
-		offset = offset || 0;
+    /**
+     * Checks if a date is in this week
+     * @param  {Date}  target date we're looking for
+     * @param  {Int}  offset day offset, used with the isNextWeek, so that we can use the same logic, but just add 7 to the equations.
+     * @return {Boolean}
+     */
+    isThisWeek: function(target, offset) {
+        offset = offset || 0;
 
-		var now = new Date(),
-			dayInMs = 1000 * 60 * 60 * 24,
-			start = (now.getDay() - this.weekStartsOn < 0 ? -6 : now.getDay() - this.weekStartsOn) + offset,
-			end = start + 6,
-			diff = Math.round(((target.getTime() - now.getTime()) / dayInMs)) + (now.getDay());
+        var now = new Date(),
+            dayInMs = 1000 * 60 * 60 * 24,
+            start = (now.getDay() - this.weekStartsOn < 0 ? -6 : now.getDay() - this.weekStartsOn) + offset,
+            end = start + 6,
+            diff = Math.round(((target.getTime() - now.getTime()) / dayInMs)) + (now.getDay());
 
-		return diff >= start && diff <= end;
-	},
+        return diff >= start && diff <= end;
+    },
 
-	/**
-	 * Gets the first day of the week.
-	 * @param  {Date} date a target date, or if not suplied, it'll be today's date.
-	 * @return {Date}      first day of the week
-	 */
-	getStartOfWeek: function(date) {
-		date = date || new Date();
+    /**
+     * Gets the first day of the week.
+     * @param  {Date} date a target date, or if not suplied, it'll be today's date.
+     * @return {Date}      first day of the week
+     */
+    getStartOfWeek: function(date) {
+        date = date || new Date();
 
-		var daysInMs = 24*60*60*1000,
-			diffToStart = (date.getDay() - this.weekStartsOn) * daysInMs;
+        var daysInMs = 24*60*60*1000,
+            diffToStart = (date.getDay() - this.weekStartsOn) * daysInMs;
 
-		date.setTime(date.getTime() - diffToStart);
-		
-		return date;
-	},
+        date.setTime(date.getTime() - diffToStart);
+        
+        return date;
+    },
 
-	/**
-	 * Is a specified date in next week
-	 * @param  {Date}  target date that we're comparing
-	 * @return {Boolean}        if it is or isnt in next week
-	 */
-	isNextWeek: function(target) {
-		return this.isThisWeek(target, 7);
-	},
+    /**
+     * Is a specified date in next week
+     * @param  {Date}  target date that we're comparing
+     * @return {Boolean}        if it is or isnt in next week
+     */
+    isNextWeek: function(target) {
+        return this.isThisWeek(target, 7);
+    },
 
-	/**
-	 * Is a supplied date today?
-	 * @param  {Date}  date the target date
-	 * @return {Boolean}      it is or it isnt
-	 */
-	isToday: function(date) {
-		return date.toDateString() === new Date().toDateString();
-	},
+    /**
+     * Is a supplied date today?
+     * @param  {Date}  date the target date
+     * @return {Boolean}      it is or it isnt
+     */
+    isToday: function(date) {
+        return date.toDateString() === new Date().toDateString();
+    },
 
-	/**
-	 * Is a supplied date tomorrow?
-	 * @param  {Date}  date is the date tomorrow
-	 * @return {Boolean}      it either is or isnt tomorrow
-	 */
-	isTomorrow: function(date) {
-		var tomorrow = new Date();
-		tomorrow.setDate(tomorrow.getDate() + 1);
+    /**
+     * Is a supplied date tomorrow?
+     * @param  {Date}  date is the date tomorrow
+     * @return {Boolean}      it either is or isnt tomorrow
+     */
+    isTomorrow: function(date) {
+        var tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
 
-		return date.toDateString() === tomorrow.toDateString();
-	},
+        return date.toDateString() === tomorrow.toDateString();
+    },
 
-	/**
-	 * Is the current time between two dates?
-	 * @param  {Date}  left  start of date range
-	 * @param  {Date}  right end of date range
-	 * @return {Boolean}       if it's in the range or not.
-	 */
-	isNowBetween: function(left, right) {
-		var now = new Date();
-		return now >= left && now <= right;
-	},
+    /**
+     * Is the current time between two dates?
+     * @param  {Date}  left  start of date range
+     * @param  {Date}  right end of date range
+     * @return {Boolean}       if it's in the range or not.
+     */
+    isNowBetween: function(left, right) {
+        var now = new Date();
+        return now >= left && now <= right;
+    },
 
-	_getDateSnippet: function(date, block) {
+    /**
+     * Simple date formatting
+     * @param  {Date} date   date we're going to format
+     * @param  {String} format formatting string
+     * @return {String}        formatted date.
+     */
+    format: function(date, format) {
+        format = format || 'full';
+        var formatted;
 
-	},
+        if ('date' === format) {
+            formatted = date.getFullYear()+'-'+pad(date.getMonth()+1) + '-' + pad(date.getDate());
+        } else if ('time' === format) {
+            formatted = pad(date.getHours()) + ':' + pad(date.getMinutes());
+        } else if ('full' === format) {
+            formatted = pad(date.getHours())+':' + pad(date.getMinutes()) + ' &mdash; ' + months[date.getMonth()] + ' ' + date.getDate() + ', '+date.getFullYear(); 
+        } else {
+            var token = '',
+                pointer = 0,
+                size = 1,
+                isAlpha = new RegExp('\\w+', 'i'),
+                parsed = '';
 
-	/**
-	 * Simple date formatting
-	 * @param  {Date} date   date we're going to format
-	 * @param  {String} format formatting string
-	 * @return {String}        formatted date.
-	 */
-	format: function(date, format) {
-		format = format || 'full';
-		var formatted;
+            while (pointer <= format.length) {
+                token = format.substring(pointer, pointer+size);
 
-		if ('date' === format) {
-			formatted = date.getFullYear()+'-'+pad(date.getMonth()+1) + '-' + pad(date.getDate());
-		} else if ('time' === format) {
-			formatted = pad(date.getHours()) + ':' + pad(date.getMinutes());
-		} else if ('full' === format) {
-			formatted = pad(date.getHours())+':' + pad(date.getMinutes()) + ' &mdash; ' + months[date.getMonth()] + ' ' + date.getDate() + ', '+date.getFullYear();	
-		} else {
-			var tokens = format.split(' '),
-				blocks = [];
+                if (isAlpha.test(token)) {
+                    if (!__[token]) {
+                        size++;
+                    } else {
+                        parsed += __[token](date);
+                        pointer = pointer + size;
+                        size = 1;
+                    }
 
-			for(var key in __) {
-				format = format.replace(key, __[key](date));
-			}
+                } else {
+                    parsed += token;
+                    pointer++;
+                    size = 1;
+                }
+            }
 
-			formatted = format;
-		}
+            formatted = parsed;
+        }
 
-		return formatted;
-	}
+        return formatted;
+    }
 };
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var datejs = require('./datejs'),
-	now = new Date();
+	now = new Date('November 20 2015');
 
 console.log(datejs.format(now, 'M'));
 console.log(datejs.format(now, 'F, Y'));
 console.log(datejs.format(now, 'mdY'));
-console.log(datejs.format(now, 'D, l'));
+console.log(datejs.format(now, 'l F d, Y'));
